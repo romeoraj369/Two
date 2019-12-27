e56f86d07cb3a76cab724f6e3398c7b84d70ea61
Raj <romeoraj0123@gmail.com>
2019-12-27 18:09:43 +0530

    NEW2 mul updated

--- mul.java
+++ mul.java
@@ -2,5 +2,5 @@ public class mul{
 public static void main(String[] args){
 int a=1;
 int b=2;
-System.out.println(" modified1 OUTPUT OF MULTIPLIED VALUES > "+(a*b));
+System.out.println(" modified2 OUTPUT OF MULTIPLIED VALUES > "+(a*b));
 }}
